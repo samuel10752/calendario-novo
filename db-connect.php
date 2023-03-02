aff3642bf05e599f18e53b424b25691413f48e40
samuel107152 <samuecauabarbosa114@gmail.com>
2023-03-02 09:54:37 -0300

    att de comentarios. novo banco e reset na pagina

--- db-connect.php
+++ db-connect.php
@@ -2,7 +2,7 @@
 $host     = 'localhost';
 $username = 'root';
 $password = '';
-$dbname   ='dummy_db';
+$dbname   ='dummy_db.sql'; //Aqui você altera o nome do banco 
 
 $conn = new mysqli($host, $username, $password, $dbname);
 if(!$conn){
